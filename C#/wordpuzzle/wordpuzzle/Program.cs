cfee410f82ab67374365ee6be1f5451c873fc790
Marius S Kristiansen <mariussk@getacademy.no>
2019-02-07 21:11:19 +0100

    bulk upload

--- C#/wordpuzzle/wordpuzzle/Program.cs
+++ C#/wordpuzzle/wordpuzzle/Program.cs
@@ -13,9 +13,21 @@ namespace wordpuzzle
         static void Main(string[] args)
         {
             string[] lines = ReadFile(filename);
-            foreach (var line in lines)
+            string[] words = parseWords(lines);
+            List<string> validWords = new List<string>();
+                
+            foreach (var V in words)
             {
-                //Console.WriteLine(line);
+                if (IsValid(V))
+                {
+                    validWords.Add(V);
+                }
+            }
+
+            string[] output = validWords.ToArray();
+            foreach (var l in output)
+            {
+                Console.WriteLine(l);
             }
         }
 
@@ -23,6 +35,38 @@ namespace wordpuzzle
         {
             string[] rawText = System.IO.File.ReadAllLines(filename);
             return rawText;
+        }
+
+        static bool IsValid(string word)
+        {
+            if (word.Length > 10 || word.Length < 7) return false;
+            if (word.Contains('-')) return false;
+            return true;
+        }
+
+        static string[] parseWords(string[] lines)
+        {
+            List<string> uniqueWords = new List<string>();
+            bool firstWord = true;
+            string lastWord = null;
+            foreach (var line in lines)
+            {
+                string[] words = line.Split('\t');
+                if (words.Length != 6) continue;
+                string word = words[1];
+                if (firstWord)
+                {
+                    lastWord = word;
+                    uniqueWords.Add(word);
+                    firstWord = false;
+                }
+                else
+                {
+                    if (word != lastWord) uniqueWords.Add(word); lastWord = word;
+                }
+            }
+
+            return uniqueWords.ToArray();
         }
     }
 }
