8b2732bf4f9b3093f52899697a121a3926cc3fde
Marius S Kristiansen <mariussk@getacademy.no>
2019-02-07 10:38:06 +0100

    Add wordpuzzle project

---
+++ C#/wordpuzzle/wordpuzzle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordpuzzle
+{
+    class Program
+    {
+        public static string filename = @"I:\GET\source\repos\AlmostGETit\C#\wordpuzzle\ordbank_bm\fullform_bm.txt";
+
+        static void Main(string[] args)
+        {
+            string[] lines = ReadFile(filename);
+            foreach (var line in lines)
+            {
+                //Console.WriteLine(line);
+            }
+        }
+
+        static string[] ReadFile(string filename)
+        {
+            string[] rawText = System.IO.File.ReadAllLines(filename);
+            return rawText;
+        }
+    }
+}
